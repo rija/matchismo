6311c1e2dc15d06472a5813eaed3b94707bce165
Rija Ménagé <github@rijam.sent.as>
2013-03-10 18:45:34 +0000

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by R Ménagé on 10/03/2013.
+//  Copyright (c) 2013 R Ménagé. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by R Ménagé on 10/03/2013.
+//  Copyright (c) 2013 R Ménagé. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
