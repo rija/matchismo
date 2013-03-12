76ff7c830061118bfb36cc3d7a421b5bbab2ee40
Rija Ménagé <github@rijam.sent.as>
2013-03-12 23:08:21 +0000

    Implemented PlayingCard

--- Matchismo/Model/PlayingCard.m
+++ Matchismo/Model/PlayingCard.m
@@ -8,6 +8,43 @@
 
 #import "PlayingCard.h"
 
+
+
 @implementation PlayingCard
+
+- (NSString *) contents
+{
+    NSArray *rankStrings = [PlayingCard rankStrings];
+    return [rankStrings[self.rank] stringByAppendingString:self.suit];
+}
+
+@synthesize suit = _suit;
+
++ (NSArray*)validSuits
+{
+    return @[@"♥",@"♦",@"♠",@"♣"];
+}
+
++ (NSArray*)rankStrings
+{
+    return @[@"?",@"A",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"J",@"Q",@"K"];
+}
+
++ (NSUInteger)maxRank
+{
+    return [self rankStrings].count-1;
+}
+- (NSString*)suit
+{
+    return _suit ? _suit : @"?";
+}
+
+
+- (void)setSuit:(NSString *)suit
+{
+    if ([[PlayingCard validSuits] containsObject:suit]) {
+        _suit = suit;
+    }
+}
 
 @end
