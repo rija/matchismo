189c1b19d68d2e83f4be5974f918f1a9b9126899
Rija Ménagé <github@rijam.sent.as>
2013-03-12 22:46:16 +0000

    Implemented generic deck

--- Matchismo/Model/Deck.m
+++ Matchismo/Model/Deck.m
@@ -8,16 +8,43 @@
 
 #import "Deck.h"
 
+@interface Deck()
+@property (strong,nonatomic) NSMutableArray *cards;
+@end
+
+
 @implementation Deck
+
+- (NSMutableArray*)cards {
+    if(!_cards)
+        _cards = [[NSMutableArray alloc]init];
+    return _cards;
+    
+}
 
 - (void)addCard:(Card *)card atTop:(BOOL)atTop
 {
-    
+    if (atTop) {
+        if (card)
+            [self.cards insertObject:card atIndex:0];
+    }
+    else {
+        if(card)
+            [self.cards addObject:card];
+    }
 }
 
 - (Card *)drawRandomCard
 {
+    Card *randomCard = nil; //unecessary as local variables start out at zero in ObjC
     
+    if(self.cards.count) {
+        unsigned index = arc4random() % self.cards.count;
+        randomCard = self.cards[index];
+        [self.cards removeObjectAtIndex:index];
+    }
+    
+    return randomCard;
 }
 
 @end
