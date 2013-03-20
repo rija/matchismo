f833a8726f0dd6d758b3b42279fcd7fe89ded2af
Rija Ménagé <git@rijam.sent.as>
2013-03-20 14:32:50 +0800

    Modified the controller to make use of the CardMatchingGame class (flipCard and cardButtons setter now use it). Removed direct use of a Deck in the controller as this is handled by the game logic in CardMatchingGame. Added a function to keep the UI updated to reflect the state of the CardMatchingGame model class. In the UI updater function, make sure we set the title for the matched card state that corresponds to the button to be both selected and disabled. In the UI updater function, set the transparency of matched cards to non-opaque so it is obvious they are matched and cannot be played anymore

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -9,41 +9,50 @@
 #import "CardGameViewController.h"
 #import "PlayingCardDeck.h"
 #import "Deck.h"
+#import "CardMatchingGame.h"
 
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipLabel;
 @property (nonatomic) int flipCount;
-@property (strong, nonatomic) Deck *deck;
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
+@property (strong, nonatomic) CardMatchingGame * game;
 @end
 
 @implementation CardGameViewController
 
+- (CardMatchingGame *) game
+{
+    if (!_game) {
+        _game = [[CardMatchingGame alloc] initWithCardCount:[self.cardButtons count]
+                                                  usingDeck:[[PlayingCardDeck alloc] init]];
+    }
+    return _game;
+}
 
 - (void) setCardButtons:(NSArray *)cardButtons
 {
     _cardButtons = cardButtons;
-    for (UIButton *cardButton in self.cardButtons) {
-        Card *card = [self.deck drawRandomCard];
-        [cardButton setTitle:card.contents forState:UIControlStateSelected];
-    }
+    [self updateUI];
 }
 
-- (Deck*) deck
+- (void) updateUI
 {
-    if (!_deck) {
-        _deck = [[PlayingCardDeck alloc]init];
+    for (UIButton *cardButton in self.cardButtons) {
+        Card *card = [self.game cardAtIndex:[self.cardButtons indexOfObject:cardButton]];
+        [cardButton setTitle:card.contents forState:UIControlStateSelected];
+        [cardButton setTitle:card.contents forState:UIControlStateSelected|UIControlStateDisabled];
+        cardButton.selected = card.isFaceUp;
+        cardButton.enabled = !card.isUnplayable;
+        cardButton.alpha = (card.isUnplayable) ? 0.3 : 1.0;
     }
     
-    return _deck;
 }
 
 - (IBAction)flipCard:(UIButton *)sender {
     
-    [sender setTitle:[[self.deck drawRandomCard] contents] forState:UIControlStateSelected];
-    
-    sender.selected = !sender.isSelected;
+    [self.game flipCardAtIndex:[self.cardButtons indexOfObject:sender]];
     self.flipCount++;
+    [self updateUI];
     
     
 }
